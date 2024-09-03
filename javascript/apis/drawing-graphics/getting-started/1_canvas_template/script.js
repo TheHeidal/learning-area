878bfeb6f51bede7c4aeac1fa0b3968627820292
Mars Heidal <theHeidal@gmail.com>
2024-09-03 09:36:26 -0700

    Practiced first steps in drawing graphics.

--- javascript/apis/drawing-graphics/getting-started/1_canvas_template/script.js
+++ javascript/apis/drawing-graphics/getting-started/1_canvas_template/script.js
@@ -1,7 +1,85 @@
-const canvas = document.querySelector('.myCanvas');
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
-const ctx = canvas.getContext('2d');
+const canvas = document.querySelector(".myCanvas");
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = 'rgb(0,0,0)';
-ctx.fillRect(0,0,width,height);
+ctx.fillStyle = "rgb(0,0,0)";
+ctx.fillRect(0, 0, width, height);
+
+
+ctx.lineTo(150, 50);
+const triHeight = 50 * Math.tan(degToRad(60));
+ctx.lineTo(100, 50 + triHeight);
+ctx.lineTo(50, 50);
+ctx.fill();
+
+
+
+
+// ctx.fillStyle = "rgb(255 0 0)";
+// ctx.beginPath();
+// ctx.moveTo(50, 50);
+// // draw your path
+// ctx.lineTo(150, 50);
+// const triHeight = 50 * Math.tan(degToRad(60));
+// ctx.lineTo(100, 50 + triHeight);
+// ctx.lineTo(50, 50);
+// ctx.fill();
+
+// ctx.fillStyle = "rgb(0 0 255)";
+// ctx.beginPath();
+// ctx.arc(150, 106, 50, degToRad(0), degToRad(360), false);
+// ctx.fill();
+
+// ctx.fillStyle = "yellow";
+// ctx.beginPath();
+// ctx.arc(200, 106, 50, degToRad(-45), degToRad(45), true);
+// ctx.lineTo(200, 106);
+// ctx.fill();
+
+// drawToken("orange", 300, 250, 100, 25, 30, 270);
+
+// ctx.strokeStyle = "white";
+// ctx.lineWidth = 1;
+// ctx.font = "36px arial";
+// ctx.strokeText("Canvas text", 50, 50);
+
+// ctx.fillStyle = "red";
+// ctx.font = "48px georgia";
+// ctx.fillText("Canvas text", 50, 150);
+
+// canvas.setAttribute("aria-label", "Canvas text");
+
+// function degToRad(degrees) {
+//   return (degrees * Math.PI) / 180;
+// }
+
+function drawToken(
+  fillstyle,
+  centerX,
+  centerY,
+  radius,
+  height,
+  arc,
+  startingAngle
+) {
+  ctx.fillStyle = fillstyle;
+  ctx.beginPath();
+  ctx.arc(
+    centerX,
+    centerY,
+    radius,
+    degToRad(startingAngle),
+    degToRad(startingAngle + arc),
+    false
+  );
+  ctx.arc(
+    centerX,
+    centerY,
+    radius - height,
+    degToRad(startingAngle + arc),
+    degToRad(startingAngle),
+    true
+  );
+  ctx.fill();
+}
--- javascript/apis/drawing-graphics/getting-started/3_canvas_paths/script.js
+++ javascript/apis/drawing-graphics/getting-started/3_canvas_paths/script.js
@@ -1,31 +1,89 @@
-const canvas = document.querySelector('.myCanvas');
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
-const ctx = canvas.getContext('2d');
+const canvas = document.querySelector(".myCanvas");
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = 'rgb(0,0,0)';
-ctx.fillRect(0,0,width,height);
+ctx.fillStyle = "rgb(0,0,0)";
+ctx.fillRect(0, 0, width, height);
 
 function degToRad(degrees) {
-  return degrees * Math.PI / 180;
-};
+  return (degrees * Math.PI) / 180;
+}
 
-ctx.fillStyle = 'rgb(255,0,0)';
+// ctx.fillStyle = "rgb(255,0,0)";
+// ctx.beginPath();
+// ctx.moveTo(50, 50);
+// ctx.lineTo(150, 50);
+// const triHeight = 50 * Math.tan(degToRad(60));
+// ctx.lineTo(100, 50 + triHeight);
+// ctx.lineTo(50, 50);
+// ctx.fill();
+
+const image = new Image();
+image.src = ""
+
+
+
+ctx.lineWidth = 3;
+
+ctx.strokeStyle = "rgb(255,0,0)";
 ctx.beginPath();
-ctx.moveTo(50,50);
-ctx.lineTo(150,50);
-const triHeight = 50 * Math.tan(degToRad(60));
-ctx.lineTo(100,50+triHeight);
-ctx.lineTo(50,50);
+ctx.arc(300, 300, 265, degToRad(0), degToRad(360), false);
+ctx.stroke();
+
+// months
+ctx.fillStyle = "rgb(255,250,250)";
+ctx.beginPath();
+ctx.arc(300, 300, 250, degToRad(0), degToRad(360), false);
 ctx.fill();
 
-ctx.fillStyle = 'rgb(0,0,255)';
-ctx.beginPath();
-ctx.arc(150, 106, 50, degToRad(0), degToRad(360), false);
-ctx.fill()
 
-ctx.fillStyle = 'yellow';
+ctx.lineWidth = 2;
+ctx.strokeStyle = "#767777";
+subdivide(300, 300, 250, 200, 12, 0);
+
+ctx.translate(300, 300);
+ctx.rotate(15);
+
+ctx.
+
+//Saturn
+ctx.fillStyle = "#1e1e1e";
 ctx.beginPath();
-ctx.arc(200, 106, 50, degToRad(-45), degToRad(45), true);
-ctx.lineTo(200,106);
+ctx.arc(300, 300, 200, degToRad(0), degToRad(360), false);
 ctx.fill();
+
+// jupiter
+ctx.fillStyle = "#dcbc95";
+ctx.beginPath();
+ctx.arc(300, 300, 180, degToRad(0), degToRad(360), false);
+ctx.fill();
+
+function subdivide(
+  centerX,
+  centerY,
+  outerRadius,
+  innerRadius,
+  divisions,
+  offsetAngle
+) {
+  for (let i = 0; i < divisions; i++) {
+    ctx.beginPath();
+    const angle = degToRad((360 / divisions) * i + offsetAngle);
+    ctx.moveTo(
+      centerX + outerRadius * Math.cos(angle),
+      centerY + outerRadius * Math.sin(angle)
+    );
+    ctx.lineTo(
+      centerX + innerRadius * Math.cos(angle),
+      centerY + innerRadius * Math.sin(angle)
+    );
+    ctx.stroke();
+  }
+}
+
+// ctx.fillStyle = "yellow";
+// ctx.beginPath();
+// ctx.arc(200, 106, 50, degToRad(-45), degToRad(45), true);
+// ctx.lineTo(200, 106);
+// ctx.fill();
